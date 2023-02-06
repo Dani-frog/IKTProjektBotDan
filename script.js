821f4f0c8406011603f5a9743af62e84a4b30094
Jakab Botond <jakab.botond@ckik.loc>
2023-02-06 12:22:37 +0100

    kezdetek

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 var jatekTer = document.getElementById("jatekter");
+
+var balPanel = document.createElement("div");
+var kartyaBox = document.createElement("div");
+var pontokBox = document.createElement("div");
+var tabla = document.createElement("div");
+var korokBox = document.createElement("div");
+
+function JatekterBetoltes()
+{
+    balPanel.appendChild(kartyaBox);
+    balPanel.appendChild(pontokBox);
+    jatekTer.appendChild(balPanel);
+    jatekTer.appendChild(tabla);
+    jatekTer.appendChild(korokBox);
+
+    kartyaBox.innerHTML = "kartyaBox";
+    pontokBox.innerHTML = "pontokBox";
+    tabla.innerHTML = "tabla";
+    korokBox.innerHTML = "korokBox";
+}
+function JatekterElrendezes()
+{
+    balPanel.id = "balpanel";
+    kartyaBox.id = "kartyabox";
+    pontokBox.id = "pontokbox";
+    tabla.id = "tabla";
+    korokBox.id = "korokbox";
+}
+function Main()
+{
+    JatekterBetoltes();
+    JatekterElrendezes();
+}
+Main();
