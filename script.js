89f66b4eed336b068b5fb4a651d09237f6beca97
Tóth Dániel János <danikataurusz@gmail.com>
2023-02-17 17:47:11 +0100

    végső jó

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var jatekTer = document.getElementById("jatekter");
-
 var balPanel = document.createElement("div");
 var kartyaBox = document.createElement("div");
 var pontokBox = document.createElement("div");
@@ -7,68 +6,49 @@ var tabla = document.createElement("div");
 var korokBox = document.createElement("div");
 
 var Kartyak = [{id:1,value:-3,sign:''},
-{id:2,value:2,sign:''},
-{id:3,value:5,sign:''},
-{id:4,value:4,sign:''},
-{id:5,value:3,sign:''},
-{id:6,value:0,sign:'pap'},
-{id:7,value:-6,sign:''},
-{id:8,value:6,sign:''},
-{id:9,value:0,sign:'taliga'},
-{id:10,value:2,sign:''},
-{id:11,value:0,sign:'hegy'},
-{id:12,value:-5,sign:''},
-{id:13,value:4,sign:''},
-{id:14,value:0,sign:'sarkany'},
-{id:15,value:5,sign:''},
-{id:16,value:6,sign:''},
-{id:17,value:-4,sign:''},
-{id:18,value:1,sign:''},
-{id:19,value:-1,sign:''},
-{id:20,value:-2,sign:''},
-{id:21,value:1,sign:''},
-{id:22,value:3,sign:''},
-{id:23,value:0,sign:'hegy'},
+                {id:2,value:2,sign:''},
+                {id:3,value:5,sign:''},
+                {id:4,value:4,sign:''},
+                {id:5,value:3,sign:''},
+                {id:6,value:0,sign:'pap'},
+                {id:7,value:-6,sign:''},
+                {id:8,value:6,sign:''},
+                {id:9,value:0,sign:'taliga'},
+                {id:10,value:2,sign:''},
+                {id:11,value:0,sign:'hegy'},
+                {id:12,value:-5,sign:''},
+                {id:13,value:4,sign:''},
+                {id:14,value:0,sign:'sarkany'},
+                {id:15,value:5,sign:''},
+                {id:16,value:6,sign:''},
+                {id:17,value:-4,sign:''},
+                {id:18,value:1,sign:''},
+                {id:19,value:-1,sign:''},
+                {id:20,value:-2,sign:''},
+                {id:21,value:1,sign:''},
+                {id:22,value:3,sign:''},
+                {id:23,value:0,sign:'hegy'},
 ]
 
 
 var Tornyok = [
 {id:1,value:1,hp:1},
-{id:2,value:2,hp:2},
-{id:3,value:3,hp:3},
-{id:4,value:4,hp:4},
-{id:5,value:1,hp:1},
+{id:2,value:1,hp:2},
+{id:3,value:1,hp:3},
+{id:4,value:1,hp:4},
+{id:5,value:2,hp:1},
 {id:6,value:2,hp:2},
-{id:7,value:3,hp:3},
-{id:8,value:4,hp:4},
-{id:9,value:1,hp:1},
-{id:10,value:2,hp:2},
+{id:7,value:2,hp:3},
+{id:8,value:2,hp:4},
+{id:9,value:3,hp:1},
+{id:10,value:3,hp:2},
 {id:11,value:3,hp:3},
-{id:12,value:4,hp:4},
-{id:13,value:1,hp:1},
-{id:14,value:2,hp:2},
-{id:15,value:3,hp:3},
+{id:12,value:3,hp:4},
+{id:13,value:4,hp:1},
+{id:14,value:4,hp:2},
+{id:15,value:4,hp:3},
 {id:16,value:4,hp:4},
 ]
-
-{
-{id:1;value:1;sign:hp:1};
-{id:2;value:2;hp:2};
-{id:3;value:3;hp:3};
-{id:4;value:4;hp:4};
-{id:5;value:1;hp:1};
-{id:6;value:2;hp:2};
-{id:7;value:3;hp:3};
-{id:8;value:4;hp:4};
-{id:9;value:1;hp:1};
-{id:10;value:2;hp:2};
-{id:11;value:3;hp:3};
-{id:12;value:4;hp:4};
-{id:13;value:1;hp:1};
-{id:14;value:2;hp:2};
-{id:15;value:3;hp:3};
-{id:16;value:4;hp:4};
-}
 
 function JatekterBetoltes()
 {
